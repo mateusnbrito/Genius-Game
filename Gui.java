23413e6302d775a0a18e70e65c85820c0bd28dcd
Mateus Brito <mateusbritoti@hotmail.com>
2021-12-15 16:50:21 -0300

    Melhorias nos botoes

--- Gui.java
+++ Gui.java
@@ -12,10 +12,10 @@ import java.awt.event.ActionListener;
 import java.util.Scanner;
 
 public class Gui extends JFrame {
-	private JTextField textField1;
-	private JTextField textField2;
-  private JTextField textField3;
-  private JTextField textField4;
+	private JTextField inputFieldNomePartida;
+	private JTextField inputFieldDataPartida;
+  private JTextField inputFieldNomeJogador;
+  private JTextField inputFieldApelidoJogador;
 
   Main main = new Main();
   Scanner scanner = new Scanner(System.in);
@@ -26,215 +26,212 @@ public class Gui extends JFrame {
 		setContentPane(contentPane);
     contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.PAGE_AXIS));
 
-		JLabel lblNomePartida = new JLabel("Nome da partida:");
-		contentPane.add(lblNomePartida);
+		JLabel labelNomePartida = new JLabel("Nome da partida:");
+		contentPane.add(labelNomePartida);
 
-    JLabel lblDataPartida = new JLabel("Data da partida:");
-		contentPane.add(lblDataPartida);
+    JLabel labelDataPartida = new JLabel("Data da partida:");
+		contentPane.add(labelDataPartida);
 
-    JLabel lblNomeJogador = new JLabel("Nome do Jogador:");
-		contentPane.add(lblNomeJogador);
+    JLabel labelNomeJogador = new JLabel("Nome do Jogador:");
+		contentPane.add(labelNomeJogador);
 
-		JLabel lblApelido = new JLabel("Apelido do Jogador:");
-		contentPane.add(lblApelido);
+		JLabel labelApelidoJogador = new JLabel("Apelido do Jogador:");
+		contentPane.add(labelApelidoJogador);
 
-		JLabel lblJogadores = new JLabel("Jogadores");
-		contentPane.add(lblJogadores);
+		JLabel labelJogadores = new JLabel("Jogadores");
+		contentPane.add(labelJogadores);
 
-		textField1 = new JTextField();
-		textField1.setColumns(10);
-		contentPane.add(textField1);
+		inputFieldNomePartida = new JTextField();
+		inputFieldNomePartida.setColumns(10);
+		contentPane.add(inputFieldNomePartida);
 
-		textField2 = new JTextField();
-		textField2.setColumns(10);
-		contentPane.add(textField2);
+		inputFieldDataPartida = new JTextField();
+		inputFieldDataPartida.setColumns(10);
+		contentPane.add(inputFieldDataPartida);
 
-    textField3 = new JTextField();
-		textField3.setColumns(10);
-		contentPane.add(textField3);
+    inputFieldNomeJogador = new JTextField();
+		inputFieldNomeJogador.setColumns(10);
+		contentPane.add(inputFieldNomeJogador);
 
-    textField4 = new JTextField();
-		textField4.setColumns(10);
-		contentPane.add(textField4);
+    inputFieldApelidoJogador = new JTextField();
+		inputFieldApelidoJogador.setColumns(10);
+		contentPane.add(inputFieldApelidoJogador);
 
-		JLabel lblNewLabel_1_1 = new JLabel("Dificuldade");
-		contentPane.add(lblNewLabel_1_1);
+		JLabel labelDificuldade = new JLabel("Dificuldade");
+		contentPane.add(labelDificuldade);
 
-		JButton btnNewButton_1 = new JButton("1");
-		contentPane.add(btnNewButton_1);
+		JButton botaoDificuldade1 = new JButton("1");
+		contentPane.add(botaoDificuldade1);
 
-		JButton btnNewButton_1_1 = new JButton("2");
-		contentPane.add(btnNewButton_1_1);
+		JButton botaoDificuldade2 = new JButton("2");
+		contentPane.add(botaoDificuldade2);
 
-		JButton btnNewButton_1_2 = new JButton("3");
-		contentPane.add(btnNewButton_1_2);
+		JButton botaoDificuldade3 = new JButton("3");
+		contentPane.add(botaoDificuldade3);
 
-		JLabel lblNewLabel_1_1_1 = new JLabel("Velocidade");
-		contentPane.add(lblNewLabel_1_1_1);
+		JLabel botaoVelocidade = new JLabel("Velocidade");
+		contentPane.add(botaoVelocidade);
 
-		JButton btnNewButton_1_3 = new JButton("1");
-		contentPane.add(btnNewButton_1_3);
+		JButton botaoVelocidade1 = new JButton("1");
+		contentPane.add(botaoVelocidade1);
 
-		JButton btnNewButton_1_1_1 = new JButton("2");
-		contentPane.add(btnNewButton_1_1_1);
+		JButton botaoVelocidade2 = new JButton("2");
+		contentPane.add(botaoVelocidade2);
 
-		JButton btnNewButton_1_2_1 = new JButton("3");
-		contentPane.add(btnNewButton_1_2_1);
+		JButton botaoVelocidade3 = new JButton("3");
+		contentPane.add(botaoVelocidade3);
 
-		JButton btnNewButton_1_1_1_1_Jogar = new JButton("Jogar");
-		contentPane.add(btnNewButton_1_1_1_1_Jogar);
+		JButton botaoIniciarJogo = new JButton("Jogar");
+		contentPane.add(botaoIniciarJogo);
 
-		JButton btnNewButton_1_1_1_1_1_Sair = new JButton("Sair");
-		contentPane.add(btnNewButton_1_1_1_1_1_Sair);
+		JButton botaoFinalizarJogo = new JButton("Sair");
+		contentPane.add(botaoFinalizarJogo);
 
-    JButton btnNewButton = new JButton("Novo Jogador");
-		contentPane.add(btnNewButton);
+    Button botaoTabuleiro1 = new Button("");
+    Button botaoTabuleiro2 = new Button("");
+    Button botaoTabuleiro3 = new Button("");
+    Button botaoTabuleiro4 = new Button("");
+    Button botaoTabuleiro5 = new Button("");
+    Button botaoTabuleiro6 = new Button("");
+    Button botaoTabuleiro7 = new Button("");
+    Button botaoTabuleiro8 = new Button("");
+    Button botaoTabuleiro9 = new Button("");
+    Button botaoTabuleiroRelatorio = new Button("Gerar Relatorio");
+    Button botaoTabuleiroFinalizar = new Button("Finalizar Jogo");
 
-    Button button = new Button("");
-    Button button_1 = new Button("");
-    Button button_1_1 = new Button("");
-    Button button_1_2 = new Button("");
-    Button button_1_3 = new Button("");
-    Button button_1_4 = new Button("");
-    Button button_1_5 = new Button("");
-    Button button_1_6 = new Button("");
-    Button button_1_7 = new Button("");
-    Button button_2 = new Button("Gerar Relatorio");
-    Button button_2_1 = new Button("Finalizar Jogo");
-
-    button.addActionListener((ActionListener) new ActionListener(){
+    botaoTabuleiro1.addActionListener((ActionListener) new ActionListener(){
       public void actionPerformed(ActionEvent event){
         main.inputBotaoSelecionado = "0";
       }
     });
 
-    button_1.addActionListener((ActionListener) new ActionListener(){
+    botaoTabuleiro2.addActionListener((ActionListener) new ActionListener(){
       public void actionPerformed(ActionEvent event){
         main.inputBotaoSelecionado = "1";
       }
     });
 
-    button_1_1.addActionListener((ActionListener) new ActionListener(){
+    botaoTabuleiro3.addActionListener((ActionListener) new ActionListener(){
       public void actionPerformed(ActionEvent event){
         main.inputBotaoSelecionado = "2";
       }
     });
 
-    button_1_2.addActionListener((ActionListener) new ActionListener(){
+    botaoTabuleiro4.addActionListener((ActionListener) new ActionListener(){
       public void actionPerformed(ActionEvent event){
         main.inputBotaoSelecionado = "3";
       }
     });
 
-    button_1_3.addActionListener((ActionListener) new ActionListener(){
+    botaoTabuleiro5.addActionListener((ActionListener) new ActionListener(){
       public void actionPerformed(ActionEvent event){
         main.inputBotaoSelecionado = "4";
       }
     });
 
-    button_1_4.addActionListener((ActionListener) new ActionListener(){
+    botaoTabuleiro6.addActionListener((ActionListener) new ActionListener(){
       public void actionPerformed(ActionEvent event){
         main.inputBotaoSelecionado = "5";
       }
     });
 
-    button_1_5.addActionListener((ActionListener) new ActionListener(){
+    botaoTabuleiro7.addActionListener((ActionListener) new ActionListener(){
       public void actionPerformed(ActionEvent event){
         main.inputBotaoSelecionado = "6";
       }
     });
 
-    button_1_6.addActionListener((ActionListener) new ActionListener(){
+    botaoTabuleiro8.addActionListener((ActionListener) new ActionListener(){
       public void actionPerformed(ActionEvent event){
         main.inputBotaoSelecionado = "7";
       }
     });
 
-    button_1_7.addActionListener((ActionListener) new ActionListener(){
+    botaoTabuleiro9.addActionListener((ActionListener) new ActionListener(){
       public void actionPerformed(ActionEvent event){
         main.inputBotaoSelecionado = "8";
       }
     });
 
-    button_2.addActionListener((ActionListener) new ActionListener(){
+    botaoTabuleiroRelatorio.addActionListener((ActionListener) new ActionListener(){
       public void actionPerformed(ActionEvent event){}
     });
 
-    button_2_1.addActionListener((ActionListener) new ActionListener(){
+    botaoTabuleiroFinalizar.addActionListener((ActionListener) new ActionListener(){
       public void actionPerformed(ActionEvent event){}
     });
 
-    btnNewButton_1.addActionListener((ActionListener) new ActionListener(){
+    botaoDificuldade1.addActionListener((ActionListener) new ActionListener(){
       String input4Content;
 
       public void actionPerformed(ActionEvent event){
-        input4Content = btnNewButton_1.getText().toString();
+        input4Content = botaoDificuldade1.getText().toString();
 
         main.inputDificuldadePartida = input4Content;
       }
     });
 
-    btnNewButton_1_1.addActionListener((ActionListener) new ActionListener(){
+    botaoDificuldade2.addActionListener((ActionListener) new ActionListener(){
       String input4Content;
 
       public void actionPerformed(ActionEvent event){
-        input4Content = btnNewButton_1_1.getText().toString();
+        input4Content = botaoDificuldade2.getText().toString();
 
         main.inputDificuldadePartida = input4Content;
       }
     });
 
-    btnNewButton_1_2.addActionListener((ActionListener) new ActionListener(){
+    botaoDificuldade3.addActionListener((ActionListener) new ActionListener(){
       String input4Content;
 
       public void actionPerformed(ActionEvent event){
-        input4Content = btnNewButton_1_2.getText().toString();
+        input4Content = botaoDificuldade3.getText().toString();
 
         main.inputDificuldadePartida = input4Content;
       }
     });
 
-    btnNewButton_1_3.addActionListener((ActionListener) new ActionListener(){
+    botaoVelocidade1.addActionListener((ActionListener) new ActionListener(){
       String input5Content;
 
       public void actionPerformed(ActionEvent event){
-        input5Content = btnNewButton_1_3.getText().toString();
+        input5Content = botaoVelocidade1.getText().toString();
 
         main.inputVelocidadePartida = input5Content;
       }
     });
 
-    btnNewButton_1_1_1.addActionListener((ActionListener) new ActionListener(){
+    botaoVelocidade2.addActionListener((ActionListener) new ActionListener(){
       String input5Content;
 
       public void actionPerformed(ActionEvent event){
-        input5Content = btnNewButton_1_1_1.getText().toString();
+        input5Content = botaoVelocidade2.getText().toString();
 
         main.inputVelocidadePartida = input5Content;
       }
     });
 
-    btnNewButton_1_2_1.addActionListener((ActionListener) new ActionListener(){
+    botaoVelocidade3.addActionListener((ActionListener) new ActionListener(){
       String input5Content;
 
       public void actionPerformed(ActionEvent event){
-        input5Content = btnNewButton_1_2_1.getText().toString();
+        input5Content = botaoVelocidade3.getText().toString();
 
         main.inputVelocidadePartida = input5Content;
       }
     });
 
-    btnNewButton_1_1_1_1_Jogar.addActionListener((ActionListener) new ActionListener(){
+    botaoIniciarJogo.addActionListener((ActionListener) new ActionListener(){
       String input0Content;
       String input1Content;
       String input2Content;
       String input3Content;
 
 			public void actionPerformed(ActionEvent event) {
-        // input0Content = textField1.getText().toString();
-        // input1Content = textField2.getText().toString();
-        // input2Content = textField3.getText().toString();
-        // input3Content = textField4.getText().toString();
+        // input0Content = inputFieldNomePartida.getText().toString();
+        // input1Content = inputFieldDataPartida.getText().toString();
+        // input2Content = inputFieldNomeJogador.getText().toString();
+        // input3Content = inputFieldApelidoJogador.getText().toString();
 
         input0Content = "Nova Partida";
         input1Content = "2021/12/15";
@@ -255,50 +252,57 @@ public class Gui extends JFrame {
         contentPane.repaint();
         contentPane.setLayout(null);
 
-        button.setBackground(Color.BLUE);
-        button.setBounds(40, 45, 91, 38);
-        add(button);
+        botaoTabuleiro1.setBackground(new Color(0, 0, 153));
+        // botaoTabuleiro1.setBackground(new Color(173, 216, 230));
+        botaoTabuleiro1.setBounds(40, 45, 91, 38);
+        add(botaoTabuleiro1);
 
-        button_1.setBackground(Color.MAGENTA);
-        button_1.setBounds(172, 45, 91, 38);
-        add(button_1);
+        botaoTabuleiro2.setBackground(new Color(255, 204 ,0));
+        // botaoTabuleiro2.setBackground(new Color(173, 216, 230));
+        botaoTabuleiro2.setBounds(172, 45, 91, 38);
+        add(botaoTabuleiro2);
 
-        button_1_1.setBackground(Color.RED);
-        button_1_1.setBounds(293, 45, 91, 38);
-        add(button_1_1);
+        botaoTabuleiro3.setBackground(new Color(255, 102, 0));
+        // botaoTabuleiro3.setBackground(new Color(255, 0, 0));
+        botaoTabuleiro3.setBounds(293, 45, 91, 38);
+        add(botaoTabuleiro3);
 
-        button_1_2.setBackground(Color.ORANGE);
-        button_1_2.setBounds(40, 115, 91, 38);
-        add(button_1_2);
+        botaoTabuleiro4.setBackground(new Color(153, 0, 0));
+        // botaoTabuleiro4.setBackground(new Color(255, 0, 0));
+        botaoTabuleiro4.setBounds(40, 115, 91, 38);
+        add(botaoTabuleiro4);
 
-        button_1_3.setBackground(Color.GREEN);
-        button_1_3.setActionCommand("");
-        button_1_3.setBounds(172, 115, 91, 38);
-        add(button_1_3);
+        botaoTabuleiro5.setBackground(new Color(0, 102, 0));
+        // botaoTabuleiro5.setBackground(new Color(0, 255, 0));
+        botaoTabuleiro5.setActionCommand("");
+        botaoTabuleiro5.setBounds(172, 115, 91, 38);
+        add(botaoTabuleiro5);
 
-        button_1_4.setForeground(Color.WHITE);
-        button_1_4.setBackground(Color.BLACK);
-        button_1_4.setBounds(293, 115, 91, 38);
-        add(button_1_4);
+        botaoTabuleiro6.setBackground(new Color(255, 20, 147));
+        // botaoTabuleiro6.setBackground(new Color(178, 58, 238));
+        botaoTabuleiro6.setBounds(293, 115, 91, 38);
+        add(botaoTabuleiro6);
 
-        button_1_5.setBackground(Color.GRAY);
-        button_1_5.setBounds(40, 185, 91, 38);
-        add(button_1_5);
+        botaoTabuleiro7.setBackground(new Color(85, 26, 139));
+        // botaoTabuleiro7.setBackground(new Color(255, 110, 199));
+        botaoTabuleiro7.setBounds(40, 185, 91, 38);
+        add(botaoTabuleiro7);
 
-        button_1_6.setBackground(Color.CYAN);
-        button_1_6.setBounds(172, 185, 91, 38);
-        add(button_1_6);
+        botaoTabuleiro8.setBackground(new Color(84, 84, 84));
+        // botaoTabuleiro8.setBackground(new Color(211, 211, 211));
+        botaoTabuleiro8.setBounds(172, 185, 91, 38);
+        add(botaoTabuleiro8);
 
-        button_1_7.setBackground(Color.PINK);
-        button_1_7.setForeground(Color.BLACK);
-        button_1_7.setBounds(293, 185, 91, 38);
-        add(button_1_7);
+        botaoTabuleiro9.setBackground(new Color(0, 0, 0));
+        // botaoTabuleiro9.setBackground(new Color(84, 84, 84));
+        botaoTabuleiro9.setBounds(293, 185, 91, 38);
+        add(botaoTabuleiro9);
 
-        button_2.setBounds(93, 252, 87, 38);
-        add(button_2);
+        botaoTabuleiroRelatorio.setBounds(93, 252, 87, 38);
+        add(botaoTabuleiroRelatorio);
 
-        button_2_1.setBounds(251, 252, 87, 38);
-        add(button_2_1);
+        botaoTabuleiroFinalizar.setBounds(251, 252, 87, 38);
+        add(botaoTabuleiroFinalizar);
 
         Thread Jogo = new Thread() {
           public void run() {
@@ -310,7 +314,7 @@ public class Gui extends JFrame {
 			}
 		});
 
-    btnNewButton_1_1_1_1_1_Sair.addActionListener((ActionListener) new ActionListener(){
+    botaoFinalizarJogo.addActionListener((ActionListener) new ActionListener(){
 			public void actionPerformed(ActionEvent event) {
         System.exit(0);
 			}
@@ -342,8 +346,6 @@ public class Gui extends JFrame {
         if(main.inputBotaoSelecionado != null){
           System.out.println("Novo valor! "+main.inputBotaoSelecionado);
         }
-
-        System.out.println("Rodando...");
         // main.getValorDaJogada();
         // System.out.println("\n");
 
