255e810d4c96358c02a22c81d46707098a44c19d
Mateus Brito <mateusbritoti@hotmail.com>
2021-12-15 11:04:02 -0300

    Logica para a GUI

--- Gui.java
+++ Gui.java
@@ -9,6 +9,7 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Scanner;
 
 public class Gui extends JFrame {
 	private JTextField textField1;
@@ -16,8 +17,10 @@ public class Gui extends JFrame {
   private JTextField textField3;
   private JTextField textField4;
 
+  Main main = new Main();
+  Scanner scanner = new Scanner(System.in);
+
 	public Gui() {
-    Main main = new Main();
 		JPanel contentPane = new JPanel();
 
 		setContentPane(contentPane);
@@ -86,6 +89,18 @@ public class Gui extends JFrame {
 
     JButton btnNewButton = new JButton("Novo Jogador");
 		contentPane.add(btnNewButton);
+
+    Button button = new Button("");
+    Button button_1 = new Button("");
+    Button button_1_1 = new Button("");
+    Button button_1_2 = new Button("");
+    Button button_1_3 = new Button("");
+    Button button_1_4 = new Button("");
+    Button button_1_5 = new Button("");
+    Button button_1_6 = new Button("");
+    Button button_1_7 = new Button("");
+    Button button_2 = new Button("Gerar Relatorio");
+    Button button_2_1 = new Button("Finalizar Jogo");
 
     btnNewButton_1.addActionListener((ActionListener) new ActionListener(){
       String input4Content;
@@ -172,68 +187,66 @@ public class Gui extends JFrame {
         main.setInputsJogadores();
         main.setNovosJogadores();
         main.setNovaPartida();
-        main.setIniciarPartida();
 
         contentPane.removeAll();
         contentPane.revalidate();
         contentPane.repaint();
         contentPane.setLayout(null);
 
-        Button button = new Button("");
         button.setBackground(Color.BLUE);
         button.setBounds(40, 45, 91, 38);
         add(button);
 
-        Button button_1 = new Button("");
         button_1.setBackground(Color.MAGENTA);
         button_1.setBounds(172, 45, 91, 38);
         add(button_1);
 
-        Button button_1_1 = new Button("");
         button_1_1.setBackground(Color.RED);
         button_1_1.setBounds(293, 45, 91, 38);
         add(button_1_1);
 
-        Button button_1_2 = new Button("");
         button_1_2.setBackground(Color.ORANGE);
         button_1_2.setBounds(40, 115, 91, 38);
         add(button_1_2);
 
-        Button button_1_3 = new Button("");
         button_1_3.setBackground(Color.GREEN);
         button_1_3.setActionCommand("");
         button_1_3.setBounds(172, 115, 91, 38);
         add(button_1_3);
 
-        Button button_1_4 = new Button("");
         button_1_4.setForeground(Color.WHITE);
         button_1_4.setBackground(Color.BLACK);
         button_1_4.setBounds(293, 115, 91, 38);
         add(button_1_4);
 
-        Button button_1_5 = new Button("");
         button_1_5.setBackground(Color.GRAY);
         button_1_5.setBounds(40, 185, 91, 38);
         add(button_1_5);
 
-        Button button_1_6 = new Button("");
         button_1_6.setBackground(Color.CYAN);
         button_1_6.setBounds(172, 185, 91, 38);
         add(button_1_6);
 
-        Button button_1_7 = new Button("");
         button_1_7.setBackground(Color.PINK);
         button_1_7.setForeground(Color.BLACK);
         button_1_7.setBounds(293, 185, 91, 38);
         add(button_1_7);
 
-        Button button_2 = new Button("Gerar Relatorio");
         button_2.setBounds(93, 252, 87, 38);
         add(button_2);
 
-        Button button_2_1 = new Button("Finalizar Jogo");
         button_2_1.setBounds(251, 252, 87, 38);
         add(button_2_1);
+
+        Thread Jogo = new Thread() {
+          public void run() {
+            iniciarPartida();
+          }
+        };
+
+        Jogo.start();
+
+        System.out.println("After");
 			}
 		});
 
@@ -248,4 +261,98 @@ public class Gui extends JFrame {
 	
 		setVisible(true);
 	}
+
+  public void iniciarPartida(){
+    while(!main.partidaFinalizada){
+      int indexPartidaAtual = ((main.novoJogo.getPartidas()).length)-1;
+      Partida atualPartida = main.novoJogo.getPartidas()[indexPartidaAtual];
+      int indexRodadaAtual = ((atualPartida.getRodadas()).length)-1;
+      Rodada atualRodada = atualPartida.getRodadas()[indexRodadaAtual];
+      int indexJogadaAtual = ((atualRodada.getJogadas()).length);
+
+      Boolean sequenciaCorreta;
+
+      while(!main.rodadaFinalizada){
+        main.setAtivarBotoes(atualRodada);
+        main.setImprimirTabuleiro();
+        main.setImprimirSequencia(atualRodada);
+
+        System.out.println("\n"+atualPartida.getJogadorDaVez().getApelido()+", insira o botão desejado (1 a 9):\n");
+        main.inputApelidoJogador = scanner.nextLine();
+        // main.getValorDaJogada();
+        // System.out.println("\n");
+
+        // novaJogada = new Jogada(indexJogadaAtual, atualPartida.getJogadorDaVez(), Integer.parseInt(inputBotaoSelecionado)-1, indexPartidaAtual, indexRodadaAtual);
+        // atualRodada.addJogada(novaJogada);
+
+        // sequenciaCorreta = Arrays.equals(Arrays.copyOf(atualRodada.getSequencia(), ((atualRodada.getSequencia()).length)-1), atualRodada.getBotoesDasJogadas());
+
+        // //Checagem da sequência
+        // if(!sequenciaCorreta){
+        //   atualRodada.setSequencia();
+        //   atualRodada.setBotoesDasJogadas();
+        //   atualPartida.setJogadorDaVez();
+        //   main.novoJogo.setBotoes();
+
+        //   if(atualPartida.getRodadaFinalizada()){
+        //     //Contabilizar os acertos
+        //     for(int i=0; i<(atualPartida.getJogadores()).length; i++){
+        //       Jogador atualJogador = atualPartida.getJogadores()[i];
+        //       int acertosJogador = 0;
+
+        //       for(int j=0; j<(atualRodada.getJogadas()).length; j++){
+        //         Jogada auxJogada = atualRodada.getJogadas()[j];
+
+        //         if(auxJogada.getAcerto()){
+        //           if(atualJogador.getNome() == auxJogada.getJogador().getNome()){
+        //             acertosJogador++;
+        //           }
+        //         }
+        //       }
+
+        //       atualJogador.setAcertos(acertosJogador);
+        //     }
+
+        //     Jogador atualJogador = atualPartida.getJogadores()[0];
+        //     Jogador jogadorVencedor = atualJogador;
+
+        //     //Declarar vencedor
+        //     for(int i=0; i<(atualPartida.getJogadores()).length; i++){
+        //       atualJogador = atualPartida.getJogadores()[i];
+
+        //       if(atualJogador.getAcertos()[0][indexRodadaAtual] > jogadorVencedor.getAcertos()[0][indexRodadaAtual]){
+        //         jogadorVencedor = atualJogador;
+        //       }
+        //     }
+
+        //     atualRodada.setJogadorVencedor(jogadorVencedor);
+
+        //     //Encerrar rodada
+        //     if(!atualRodada.getRodadaEmpatada()){
+        //       rodadaFinalizada = true;
+        //     }
+        //     else{
+        //       //Sugerir desempate
+        //       rodadaFinalizada = true;
+        //     }
+        //   }
+        // }
+        // else{
+        //   novaJogada.setAcerto();
+        // }
+      }
+
+      if(main.rodadaFinalizada){
+        indexRodadaAtual++;
+
+        atualPartida.addRodada(new Rodada(indexRodadaAtual, atualRodada.getDificuldade(), atualRodada.getVelocidade()));
+
+        indexRodadaAtual = ((atualPartida.getRodadas()).length)-1;
+        atualRodada = atualPartida.getRodadas()[indexRodadaAtual];
+        indexJogadaAtual = ((atualRodada.getJogadas()).length);
+
+        main.rodadaFinalizada = false;
+      }
+    }
+  }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.awt.EventQueue;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String args[]){
@@ -14,6 +15,8 @@ public class Main {
 			}
 		});
   }
+
+  Scanner scanner = new Scanner(System.in);
 
   public String inputNomePartida;
   public String inputDataPartida;
@@ -33,6 +36,7 @@ public class Main {
 
   Jogo novoJogo;
   Partida novaPartida;
+  Jogada novaJogada;
 
   public void setInputsJogadores(){
     inputPartida = new InputPartida(inputNomePartida, inputDataPartida, Integer.parseInt(inputDificuldadePartida), Integer.parseInt(inputVelocidadePartida));
@@ -52,159 +56,56 @@ public class Main {
     novoJogo = new Jogo(novaPartida);
   }
 
-  //Início do Gameplay
-  public void setIniciarPartida(){
-    while(!partidaFinalizada){
-      int indexPartidaAtual = ((novoJogo.getPartidas()).length)-1;
-      Partida atualPartida = novoJogo.getPartidas()[indexPartidaAtual];
-      int indexRodadaAtual = ((atualPartida.getRodadas()).length)-1;
-      Rodada atualRodada = atualPartida.getRodadas()[indexRodadaAtual];
-      int indexJogadaAtual = ((atualRodada.getJogadas()).length);
-      
-      Boolean sequenciaCorreta;
-
-      while(!rodadaFinalizada){
-        //Ativar botoes
-        for(int i=0; i<((novoJogo.getBotoes()).length); i++){
-          for(int j=0; j<(atualRodada.getSequencia()).length; j++){
-            if((novoJogo.getBotoes()[i].id == atualRodada.getSequencia()[j])){
-              novoJogo.getBotoes()[i].setFoiAtivado(true, novoJogo.getVelocidade(atualRodada.getVelocidade()));
-            }
-          }
-        }
-
-        //Imprimir tabuleiro
-        for(int i=0; i<((novoJogo.getBotoes()).length); i++){
-          String[] botaoPressionado = {" ", " ", " "};
-
-          if(i%3 == 0){
-            int auxCont = 0;
-
-            for(int j=i; j<i+3; j++){
-              Botao atualBotao = (novoJogo.getBotoes())[j];
-
-              if(atualBotao.getFoiAtivado()){
-                botaoPressionado[auxCont] = "X";
-              }
-
-              auxCont++;
-            }
-
-            if(i == 0){
-              System.out.println("-------------");
-            }
-
-            System.out.println("| "+botaoPressionado[0]+" | "+botaoPressionado[1]+" | "+botaoPressionado[2]+" |");
-
-            System.out.println("-------------");
-          }
-        }
-
-        System.out.println("\n");
-
-        //Botões da Sequência
-        for(int j=0; j<(atualRodada.getSequencia()).length; j++){
-          System.out.println((atualRodada.getSequencia()[j])+1);
-        }
-
-        System.out.println("\n");
-
-        //Criar função para jogar
-
-        System.out.println("\n"+atualPartida.getJogadorDaVez().getApelido()+", insira o botão desejado (1 a 9):\n");
-        // inputBotaoSelecionado = scanner.nextLine();
-        System.out.println("\n");
-
-        Jogada novaJogada = new Jogada(indexJogadaAtual, atualPartida.getJogadorDaVez(), Integer.parseInt(inputBotaoSelecionado)-1, indexPartidaAtual, indexRodadaAtual);
-        atualRodada.addJogada(novaJogada);
-
-        sequenciaCorreta = Arrays.equals(Arrays.copyOf(atualRodada.getSequencia(), ((atualRodada.getSequencia()).length)-1), atualRodada.getBotoesDasJogadas());
-
-        //Checagem da sequência
-        if(!sequenciaCorreta){
-          atualRodada.setSequencia();
-          atualRodada.setBotoesDasJogadas();
-          atualPartida.setJogadorDaVez();
-          novoJogo.setBotoes();
-
-          if(atualPartida.getRodadaFinalizada()){
-            //Contabilizar os acertos
-            for(int i=0; i<(atualPartida.getJogadores()).length; i++){
-              Jogador atualJogador = atualPartida.getJogadores()[i];
-              int acertosJogador = 0;
-
-              for(int j=0; j<(atualRodada.getJogadas()).length; j++){
-                Jogada auxJogada = atualRodada.getJogadas()[j];
-
-                if(auxJogada.getAcerto()){
-                  if(atualJogador.getNome() == auxJogada.getJogador().getNome()){
-                    acertosJogador++;
-                  }
-                }
-              }
-
-              atualJogador.setAcertos(acertosJogador);
-            }
-
-            Jogador atualJogador = atualPartida.getJogadores()[0];
-            Jogador jogadorVencedor = atualJogador;
-
-            //Declarar vencedor
-            for(int i=0; i<(atualPartida.getJogadores()).length; i++){
-              atualJogador = atualPartida.getJogadores()[i];
-
-              if(atualJogador.getAcertos()[0][indexRodadaAtual] > jogadorVencedor.getAcertos()[0][indexRodadaAtual]){
-                jogadorVencedor = atualJogador;
-              }
-            }
-
-            atualRodada.setJogadorVencedor(jogadorVencedor);
-
-            //Encerrar rodada
-            if(!atualRodada.getRodadaEmpatada()){
-              rodadaFinalizada = true;
-            }
-            else{
-              //Sugerir desempate
-              rodadaFinalizada = true;
-            }
-          }
-        }
-        else{
-          novaJogada.setAcerto();
+  public void setAtivarBotoes(Rodada _atualRodada){
+    for(int i=0; i<((novoJogo.getBotoes()).length); i++){
+      for(int j=0; j<(_atualRodada.getSequencia()).length; j++){
+        if((novoJogo.getBotoes()[i].id == _atualRodada.getSequencia()[j])){
+          novoJogo.getBotoes()[i].setFoiAtivado(true, novoJogo.getVelocidade(_atualRodada.getVelocidade()));
         }
       }
+    }
+  }
 
-      if(rodadaFinalizada){
-        indexRodadaAtual++;
+  public void setImprimirTabuleiro(){
+    for(int i=0; i<((novoJogo.getBotoes()).length); i++){
+      String[] botaoPressionado = {" ", " ", " "};
 
-        atualPartida.addRodada(new Rodada(indexRodadaAtual, atualRodada.getDificuldade(), atualRodada.getVelocidade()));
+      if(i%3 == 0){
+        int auxCont = 0;
 
-        indexRodadaAtual = ((atualPartida.getRodadas()).length)-1;
-        atualRodada = atualPartida.getRodadas()[indexRodadaAtual];
-        indexJogadaAtual = ((atualRodada.getJogadas()).length);
+        for(int j=i; j<i+3; j++){
+          Botao atualBotao = (novoJogo.getBotoes())[j];
 
-        rodadaFinalizada = false;
+          if(atualBotao.getFoiAtivado()){
+            botaoPressionado[auxCont] = "X";
+          }
+
+          auxCont++;
+        }
+
+        if(i == 0){
+          System.out.println("-------------");
+        }
+
+        System.out.println("| "+botaoPressionado[0]+" | "+botaoPressionado[1]+" | "+botaoPressionado[2]+" |");
+
+        System.out.println("-------------");
       }
     }
 
-    // scanner.close();
+    System.out.println("\n");
+  }
 
-    // while(Integer.parseInt(inputCriarJogador) == 0){
-    // // System.out.println("Insira o nome do novo jogador:");
-    // // inputNomeJogador = scanner.nextLine();
-    // inputNomeJogador = "Lucas Bispo";
+  public void setImprimirSequencia(Rodada _atualRodada){
+    //Botões da Sequência
+    for(int j=0; j<(_atualRodada.getSequencia()).length; j++){
+      System.out.println((_atualRodada.getSequencia()[j])+1);
+    }
 
-    // // System.out.println("Insira o apelido do novo jogador:");
-    // // inputApelidoJogador = scanner.nextLine();
-    // inputApelidoJogador = "Homem de Pau";
+    System.out.println("\n");
+  }
 
-    // inputsJogadores = Arrays.copyOf(inputsJogadores, (inputsJogadores.length)+1);
-    // inputsJogadores[(inputsJogadores.length)-1] = new InputJogador(inputNomeJogador, inputApelidoJogador);
-
-    // // System.out.println("\nPressione '0' para criar um novo jogador ou '1' para iniciar a partida:\n");
-    // // inputCriarJogador = scanner.nextLine();
-    // inputCriarJogador = "1";
-    // }
+  public void getValorDaJogada(){
+    inputBotaoSelecionado = scanner.nextLine();
   }
 }
